4a1f07938d11fd2b63a47a6476361884225f3baf
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-08-27 21:19:32 +0100

    test: set up playwright

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react';  // Add this line
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
